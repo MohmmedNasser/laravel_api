3809cef7e0877db2ad8a22fa582941b13a7cbd71
Mohammed Nasser <31956229+MohmmedNasser@users.noreply.github.com>
2026-07-18 15:10:16 +0300

    Initialize Laravel project structure

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+// Route::get('/', function () {
+//     return 'API Route';
+// });
+
+Route::apiResource('posts', PostController::class);
